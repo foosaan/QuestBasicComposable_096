3593700579d4064c5d180f2304a77a023990aa82
fauzan alfikri <112147642+foosaan@users.noreply.github.com>
2024-10-20 08:48:37 +0700

    Memberika border dengan ketebalan 3, warna biru dan norder melengkung

--- app/src/main/java/com/example/compossablelayout/MainActivity.kt
+++ app/src/main/java/com/example/compossablelayout/MainActivity.kt
@@ -5,17 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -84,11 +87,13 @@ fun BasicCompos(modifier: Modifier = Modifier){
         Image(painter = painterResource(id= R.drawable.fsn),
             contentDescription = null,
             modifier = Modifier
-                .size(350.dp)
-
+                .size(300.dp)
+                .border(
+                    width = 3.dp,
+                    color = Color.Blue,
+                    shape = RoundedCornerShape(12.dp)
                 )
-
-            )
+                )
     }
 }
 
